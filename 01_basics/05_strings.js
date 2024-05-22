4c826075c18d706113832656f3dad3e769b9a41c
iamDK47 <daniyalkamran33@gmail.com>
2024-05-22 12:51:02 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "dani"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Dani.ya-l-ME')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toLowerCase());
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('y'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 4)
+
+// console.log(anotherString)
+
+// const newStringOne = "   daniyal   "
+// console.log(newStringOne)
+// console.log(newStringOne.trimStart());
+
+const url = "https:/hitesh.com/hitesh%20choudhary"
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('ou'))
+
+console.log(gameName.split('.'))
