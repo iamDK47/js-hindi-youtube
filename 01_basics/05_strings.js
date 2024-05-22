280cc126870fa8179c56741857d999147cb56e06
iamDK47 <daniyalkamran33@gmail.com>
2024-05-22 14:21:19 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -7,7 +7,7 @@ const repoCount = 50
 
 const gameName = new String('Dani.ya-l-ME')
 
-// console.log(gameName[0]);
+ console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 // console.log(gameName.toLowerCase());
