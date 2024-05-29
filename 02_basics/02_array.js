fc0db447bbd7f5b01077a7be7d45ff49468895bb
iamDK47 <daniyalkamran33@gmail.com>
2024-05-29 08:55:19 +0000

    arrays

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -18,14 +18,15 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const real_another_array = another_array.flat(Infinity)
 // console.log(real_another_array);
 
-console.log(Array.isArray("daniyal"))
-console.log(Array.from("daniyal"))
-console.log(Array.from({name: "daniyal"})) // interesting
+// console.log(Array.isArray("daniyal"))
+// console.log(Array.from("daniyal"))
+// console.log(Array.from({name: "daniyal"})) // interesting
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
 
-console.log(Array.of(score1, score2, score3)); 
+// console.log(Array.of(score1, score2, score3)); 
 
 // array = is , of , from study 
+
