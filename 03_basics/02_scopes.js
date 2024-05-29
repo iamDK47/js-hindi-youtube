14f85104965f544a8257efe47aeb6044c3a42623
iamDK47 <daniyalkamran33@gmail.com>
2024-05-29 08:55:04 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,51 @@
+// let a = 10
+// const b = 20
+// var c = 300
+
+if (true){
+    let a = 20
+    const b = 30
+    var c = 40
+    // console.log(a)
+}
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one (){
+    const username = "daniyal"
+
+    function two (){
+        const website = "youtube"
+        // console.log(username);
+    } 
+    // console.log(website)
+    two()
+}
+// one()
+
+if (true) {
+    const username = "daniyal"
+    if (username === "daniyal") {
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+// +++++++++++++++++++++ interesting ++++++++++++++++++++
+
+
+console.log(addOne(5))
+
+function addOne (num){
+    
+    return num + 1
+}
+
+// console.log(addTwo(5))
+
+const addTwo = function (num){
+    return num + 2 
+}
