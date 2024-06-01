283f0a23763f91f866ef6311dc325a571bbfb8bc
iamDK47 <daniyalkamran33@gmail.com>
2024-06-01 18:20:31 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,31 @@
+// while (condition){
+
+// }
+
+// let index = 0
+// while (index <= 10 ){
+//     console.log(`value of index is ${index}`)
+//         index = index + 2
+// }
+
+// let myArray = ["daniyal", "khan", "john", "biden"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`the name is ${myArray[arr]}`)
+//     arr = arr + 1
+// }
+
+
+// let score = 1
+
+// do {
+//     console.log(`score is ${score}`)
+//     score++
+// } while (score <= 10);
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`)
+    score++
+} while (score <= 10);
