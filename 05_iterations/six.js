f47b7a05895d6e5106a9a7597f637e22f3d62f0e
iamDK47 <daniyalkamran33@gmail.com>
2024-06-03 13:22:18 +0000

    filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,37 @@
+const myCoding = ["js", "ruby", "Py", "html", "css"]
+
+// const values = myCoding.forEach( val => {
+//     console.log(val)
+//     return val
+// })
+// console.log(values)
+
+// forEach does not return value
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNum = myNums.filter( (val) => (val>4))
+// console.log(newNum)
+
+// const newNum = myNums.filter( function (val) {
+//     return val>4
+// })
+// console.log(newNum)
+
+// const newNum = myNums.filter( val => {
+//     return val>4
+// })
+// console.log(newNum)
+
+// filter loop vs if statements ++++++++++++++
+
+// const newNum = []
+
+// myNums.forEach ( (val) => {
+//     if (val>4) {
+//         newNum.push(val)
+//     }
+// })
+// console.log(newNum);
+
+
