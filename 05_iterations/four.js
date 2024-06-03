167ef3d5ab29dae1e484aa9b2ebce8e7b479ac45
iamDK47 <daniyalkamran33@gmail.com>
2024-06-03 09:00:37 +0000

    for in

---
+++ 05_iterations/four.js
@@ -0,0 +1,24 @@
+const myObj = {
+    Cpp: "C++",
+    Rb: "Ruby",
+    Py: "Python",
+    Js: "JavaScript",
+}
+
+// for (const obj of myObj) {
+//     console.log(obj)
+// }
+
+for (const key in myObj) {
+    // console.log(myObj[key])
+    // console.log(`${key} is shortcut for ${myObj[key]}`)
+}
+
+const programming = ["js", "rb", "py", "cpp"]
+
+for (const key in programming) {
+    // console.log(key)
+    console.log(programming[key])
+}
+// main diff bw forin & forof is direct printing of keys and values
+// main diff bw object and array is arr has automatic keys from 0
