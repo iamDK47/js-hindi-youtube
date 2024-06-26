418cfbe6790d8f62d02a2d3749e57a9f7ab0a0eb
iamDK47 <daniyalkamran33@gmail.com>
2024-06-26 07:43:06 +0000

    practc

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,7 +1,7 @@
 const myNumber = [1,2,3,4,5,6,7,8,9,10]
 
-// const newNums = myNumber.map( val => val+10)
-// console.log(newNums)
+const newNums = myNumber.map( val => val+10)
+console.log(newNums)
 
 // const newNums = myNumber.forEach( val => val + 10)
 // console.log(newNums)
@@ -9,9 +9,9 @@ const myNumber = [1,2,3,4,5,6,7,8,9,10]
 // const newNums = myNumber.filter( val => val + 4)
 // console.log(newNums)
 
-const newNum = myNumber
-                .map(val => val * 10 )
-                .map(val => val + 1 )
-                .filter(val => val >= 40 )
+// const newNum = myNumber
+//                 .map(val => val * 10 )
+//                 .map(val => val + 1 )
+//                 .filter(val => val >= 40 )
 
- console.log(newNum)
+//  console.log(newNum)
