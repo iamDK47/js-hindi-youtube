46aa94bbd1b4b4084b34b9d856b9da42f5ea847e
iamDK47 <daniyalkamran33@gmail.com>
2024-07-15 22:41:11 +0000

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -19,8 +19,39 @@ new Promise(function(resolve,reject){
         resolve()
     },2000)
 }).then(function(){
-    console.log("promise consumed")
+    // console.log("promise consumed")
 })
 
 // +++++++
 
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout( function(){
+        resolve({unername : "daniyal" , ID : "2222"})
+    }, 3000)
+})
+
+promiseThree.then(function (user){
+    // console.log(user)
+})
+
+// +++++++
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function() {
+        let error = true
+        if(!error){
+            resolve({username : "daniyal" , ID : "123"})
+        } else {
+            reject("error: wrong id")
+        }
+    }, 1000);
+})
+
+promiseFour.then(function(user){
+    console.log(user)
+    return user.username
+}).then(function(username){
+    console.log(username)
+}).catch(function(error){
+    console.log(error)
+})
